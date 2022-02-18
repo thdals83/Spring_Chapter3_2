25573dd1b864d0934864dc612f2cf0f065365aea
thdals83 <thdals83@naver.com>
2022-02-18 18:14:12 +0900

    first

---
+++ src/main/java/com/sparta/spring_chapter3_2/SpringChapter32Application.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_chapter3_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChapter32Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringChapter32Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/spring_chapter3_2/SpringChapter32ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_chapter3_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChapter32ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
