8129ca8690e55f585acd2c5feb99d92fcde26efa
thdals83 <thdals83@naver.com>
2022-02-21 15:36:18 +0900

    세션유지 게시글, 좋아요 구현중

---
+++ src/main/java/com/sparta/spring_chapter3_2/controller/PostRestController.java
@@ -0,0 +1,48 @@
+package com.sparta.spring_chapter3_2.controller;
+
+
+import com.sparta.spring_chapter3_2.dto.PostRequestDTO;
+import com.sparta.spring_chapter3_2.dto.UserReturnDTO;
+import com.sparta.spring_chapter3_2.model.Post;
+import com.sparta.spring_chapter3_2.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequiredArgsConstructor
+public class PostRestController {
+    private final PostRepository postRepository;
+
+    @PostMapping("/api/post") //게시물 등록
+    public UserReturnDTO createpost(@RequestBody PostRequestDTO requestDTO, HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        UserReturnDTO returnDTO = new UserReturnDTO();
+
+        //세션 확인
+        if (session.getAttribute("res") == null) {
+            returnDTO.setResult(false);
+            returnDTO.setMsg("로그인을 해주세요");
+            return returnDTO;
+        }
+        Post post = new Post(requestDTO);
+        postRepository.save(post);
+        returnDTO.setResult(true);
+        returnDTO.setMsg("success");
+
+        return returnDTO;
+    }
+
+    @GetMapping("/api/post") //게시물 조회
+    public List<Post> getPost(){
+        return
+    }
+
+
+}
---
+++ src/main/java/com/sparta/spring_chapter3_2/dto/PostRequestDTO.java
@@ -0,0 +1,16 @@
+package com.sparta.spring_chapter3_2.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+
+@Getter
+@Setter
+public class PostRequestDTO {
+
+    private String contents; //내용
+    private String username;
+    private String image; //내용
+
+}
