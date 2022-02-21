ecc2c4a9631834a131dc528f79ac35ba9548ab8a
thdals83 <thdals83@naver.com>
2022-02-22 00:20:23 +0900

    게시글, 좋아요 CRUD 완료

--- src/main/java/com/sparta/spring_chapter3_2/repository/LikeNumberRepository.java
+++ src/main/java/com/sparta/spring_chapter3_2/repository/LikeNumberRepository.java
@@ -4,5 +4,9 @@ package com.sparta.spring_chapter3_2.repository;
 import com.sparta.spring_chapter3_2.model.LikeNumber;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LikeNumberRepository extends JpaRepository<LikeNumber, Long> {
+    List<LikeNumber> findByPostIdAndUserId(Long postId, Long userId);
+
 }
