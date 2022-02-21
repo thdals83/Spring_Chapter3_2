11945cf1dfe7fdd1dca4a4aaad4fbfde41e0525c
thdals83 <thdals83@naver.com>
2022-02-22 00:20:44 +0900

    게시글, 좋아요 CRUD 완료

--- src/main/java/com/sparta/spring_chapter3_2/controller/LikeRestController.java
+++ src/main/java/com/sparta/spring_chapter3_2/controller/LikeRestController.java
@@ -1,9 +1,14 @@
 package com.sparta.spring_chapter3_2.controller;
 
 
+import com.sparta.spring_chapter3_2.dto.LikeRequestDTO;
 import com.sparta.spring_chapter3_2.dto.UserLoginDTO;
 import com.sparta.spring_chapter3_2.dto.UserRequestDTO;
+import com.sparta.spring_chapter3_2.dto.UserReturnDTO;
+import com.sparta.spring_chapter3_2.repository.LikeNumberRepository;
+import com.sparta.spring_chapter3_2.service.LikeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,19 +19,32 @@ import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @RestController
-
 public class LikeRestController {
-    @PostMapping("/api/like")
-    // 들어가는 것들 바꾸기
-    public String clicklike(@RequestBody UserLoginDTO loginDTO, HttpServletRequest request){
-        HttpSession session = request.getSession();
-//        System.out.println(session);
-        if (session.getAttribute("res") == null){
-            return "fail";
-        }
+    private final LikeNumberRepository likeNumberRepository;
+    private final LikeService likeService;
 
-        // 여기 바꾸기
-        return "succes";
+    //게시글 좋아요
+    @PostMapping("/api/like")
+    public UserReturnDTO clicklike(@RequestBody LikeRequestDTO likeRequestDTO, HttpServletRequest request){
+        //세션 존재 확인
+        HttpSession session = request.getSession();
+        UserReturnDTO returnDTO = new UserReturnDTO();
+        if (session.getAttribute("res") == null){
+            returnDTO.setResult(false);
+            returnDTO.setMsg("로그인을 해주세요");
+            return returnDTO;
+        }
+        return likeService.pluslike(likeRequestDTO);
     }
+
+    //어차피 로그인 해야 좋아요 보여서 세션 존재 확인 스킵
+    @DeleteMapping("/api/like")
+    public UserReturnDTO deletelike(@RequestBody LikeRequestDTO likeRequestDTO){
+        UserReturnDTO returnDTO = new UserReturnDTO();
+
+
+        return likeService.dellike(likeRequestDTO);
+    }
+
 
 }
