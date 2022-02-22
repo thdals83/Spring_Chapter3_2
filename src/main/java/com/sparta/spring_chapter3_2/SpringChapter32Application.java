cf793d60ed96c097f27de0ceea0152e5957fc935
thdals83 <thdals83@naver.com>
2022-02-22 15:17:35 +0900

    CORS 적용

--- src/main/java/com/sparta/spring_chapter3_2/SpringChapter32Application.java
+++ src/main/java/com/sparta/spring_chapter3_2/SpringChapter32Application.java
@@ -2,7 +2,10 @@ package com.sparta.spring_chapter3_2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJpaAuditing // 시간 자동 변경 가능하게
 @SpringBootApplication
@@ -11,6 +14,15 @@ public class SpringChapter32Application {
     public static void main(String[] args) {
         SpringApplication.run(SpringChapter32Application.class, args);
 
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000/");
+            }
+        };
     }
 
 }
--- src/main/java/com/sparta/spring_chapter3_2/controller/UserRestController.java
+++ src/main/java/com/sparta/spring_chapter3_2/controller/UserRestController.java
@@ -7,13 +7,12 @@ import com.sparta.spring_chapter3_2.dto.UserReturnDTO;
 import com.sparta.spring_chapter3_2.dto.UserRequestDTO;
 import com.sparta.spring_chapter3_2.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.swing.*;
 import java.io.IOException;
 
 
@@ -23,22 +22,50 @@ public class UserRestController {
     private final UserService userService;
 
     @PostMapping("/api/register") //유저 회원가입
-    public UserReturnDTO createuser(@RequestBody UserRequestDTO requestDTO){
+    public UserReturnDTO createuser(@RequestBody UserRequestDTO requestDTO, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        UserReturnDTO returnDTO = new UserReturnDTO();
+
+        //세션 확인
+        if (session.getAttribute("res") != null) {
+            returnDTO.setResult(false);
+            returnDTO.setMsg("이미 로그인 되어있습니다.");
+            return returnDTO;
+        }
+
         //회원가입 확인
         return userService.checkRegister(requestDTO);
     }
 
     @PostMapping("/api/login") //유저 로그인
-    public LoginReturnDTO checklogin(@RequestBody UserLoginDTO loginDTO, HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public LoginReturnDTO checklogin(@RequestBody UserLoginDTO loginDTO, HttpServletRequest request) throws IOException {
         //로그인 확인
         LoginReturnDTO res = userService.checklogin(loginDTO);
+        HttpSession session = request.getSession();
+        UserReturnDTO returnDTO = new UserReturnDTO();
+        //세션 확인
+        if (session.getAttribute("res") != null) {
+            res.setNickName(null);
+            res.setUsername(null);
+            res.setResult(false);
+            res.setMsg("이미 로그인 되어있습니다.");
+            return res;
+        }
 
-        if (res.getResult()){
-            HttpSession session = request.getSession();
-            session.setAttribute("res",res);
-            System.out.println(session.getAttribute("res"));
+        if (res.getResult()) {
+            session.setAttribute("res", res);
 
         }
         return res;
     }
+
+    @RestController
+    public class GreetingController {
+        @GetMapping("/hello")
+        public String hello() {
+            return "안녕하세요?";
+        }
+    }
+
+
 }
