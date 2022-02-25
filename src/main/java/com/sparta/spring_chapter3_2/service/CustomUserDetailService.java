fdd63010f98dfadf84806e8061c39721871db20b
thdals83 <thdals83@naver.com>
2022-02-25 16:36:36 +0900

    AS-IS ) 스프링 시큐리티 JWT 회원가입 구현 TO-BE ) 스프링 시큐리티 JWT 로그인 구현

---
+++ src/main/java/com/sparta/spring_chapter3_2/service/CustomUserDetailService.java
@@ -0,0 +1,21 @@
+package com.sparta.spring_chapter3_2.service;
+
+
+import com.sparta.spring_chapter3_2.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username); //예외처리 추후 문제 생기면 추가하기
+    }
+}
