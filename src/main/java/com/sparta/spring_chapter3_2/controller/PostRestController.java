dc9b8673e14a3824ff74cf59ebf0b22e428031af
thdals83 <thdals83@naver.com>
2022-02-26 16:25:40 +0900

    AS-IS ) 스프링 시큐리티 JWT 구현 TO-BE ) 오류 수정

--- src/main/java/com/sparta/spring_chapter3_2/controller/PostRestController.java
+++ src/main/java/com/sparta/spring_chapter3_2/controller/PostRestController.java
@@ -1,12 +1,13 @@
 package com.sparta.spring_chapter3_2.controller;
 
 
-import com.sparta.spring_chapter3_2.dto.PostRemoveRequestDTO;
-import com.sparta.spring_chapter3_2.dto.PostRequestDTO;
-import com.sparta.spring_chapter3_2.dto.PostUpdateRequestDTO;
-import com.sparta.spring_chapter3_2.dto.UserReturnDTO;
+import com.sparta.spring_chapter3_2.dto.*;
+import com.sparta.spring_chapter3_2.model.LikeNumber;
 import com.sparta.spring_chapter3_2.model.Post;
+import com.sparta.spring_chapter3_2.model.User;
+import com.sparta.spring_chapter3_2.repository.LikeNumberRepository;
 import com.sparta.spring_chapter3_2.repository.PostRepository;
+import com.sparta.spring_chapter3_2.repository.UserRepository;
 import com.sparta.spring_chapter3_2.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +20,7 @@ import java.util.List;
 public class PostRestController {
     private final PostRepository postRepository;
     private final PostService postService;
+    private final LikeNumberRepository likeNumberRepository;
 
     @PostMapping("/api/post") //게시물 등록
     public UserReturnDTO createpost(@RequestBody PostRequestDTO requestDTO) {
@@ -33,9 +35,17 @@ public class PostRestController {
     }
 
     //좋아요 다만들면 리턴 좋아요도 같이 해주기
-    @GetMapping("/api/post") //게시물 조회
-    public List<Post> getPost(){
-        return postRepository.findAllByOrderByModifiedAtDesc();
+    @PostMapping("/api/showpost") //게시물 조회
+    public LoginGetReturnDTO getPost(@RequestBody PostgetDTO postgetDTO){
+        //아이디 없을 때
+        if (postgetDTO.getUserId() == null){
+            List<LikeNumber> liketable = null;
+            LoginGetReturnDTO loginGetReturnDTO = new LoginGetReturnDTO(postRepository.findAllByOrderByModifiedAtDesc(), liketable);
+            return loginGetReturnDTO;
+        }
+        List<LikeNumber> liketable = likeNumberRepository.findByUserId(postgetDTO.getUserId());
+        LoginGetReturnDTO loginGetReturnDTO = new LoginGetReturnDTO(postRepository.findAllByOrderByModifiedAtDesc(), liketable);
+        return loginGetReturnDTO;
     }
 
     //게시물 수정
---
+++ src/main/java/com/sparta/spring_chapter3_2/dto/PostgetDTO.java
@@ -0,0 +1,14 @@
+package com.sparta.spring_chapter3_2.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostgetDTO {
+    private Long userId;
+
+}
