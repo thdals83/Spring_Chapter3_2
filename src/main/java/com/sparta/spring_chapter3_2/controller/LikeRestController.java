ae1a262f664cb41bf788d577986ce9e984da3c8a
thdals83 <thdals83@naver.com>
2022-02-26 16:23:33 +0900

    AS-IS ) 스프링 시큐리티 JWT 구현 TO-BE ) 오류 수정

--- src/main/java/com/sparta/spring_chapter3_2/controller/LikeRestController.java
+++ src/main/java/com/sparta/spring_chapter3_2/controller/LikeRestController.java
@@ -28,13 +28,11 @@ public class LikeRestController {
         return likeService.pluslike(likeRequestDTO);
     }
 
-    //어차피 로그인 해야 좋아요 보여서 세션 존재 확인 스킵
-    @DeleteMapping("/api/like")
-    public UserReturnDTO deletelike(@RequestBody LikeRequestDTO likeRequestDTO){
-        UserReturnDTO returnDTO = new UserReturnDTO();
-
-        return likeService.dellike(likeRequestDTO);
-    }
-
-
+//    //어차피 로그인 해야 좋아요 보여서 세션 존재 확인 스킵
+//    @PostMapping("/api/like")
+//    public UserReturnDTO deletelike(@RequestBody LikeRequestDTO likeRequestDTO){
+//        UserReturnDTO returnDTO = new UserReturnDTO();
+//
+//        return likeService.dellike(likeRequestDTO);
+//    }
 }
