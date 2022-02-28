4f9449395be5d873a61f98465bc9718c43559185
thdals83 <thdals83@naver.com>
2022-02-28 21:57:00 +0900

    AS-IS ) LikeNumber, User 엔티티 연관관계 작업 TO-BE ) LikeNUmber, Post 엔티티 연관관계 작업

--- src/main/java/com/sparta/spring_chapter3_2/dto/LoginGetReturnDTO.java
+++ src/main/java/com/sparta/spring_chapter3_2/dto/LoginGetReturnDTO.java
@@ -1,6 +1,5 @@
 package com.sparta.spring_chapter3_2.dto;
 
-import com.sparta.spring_chapter3_2.model.LikeNumber;
 import com.sparta.spring_chapter3_2.model.Post;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,7 +12,7 @@ import java.util.List;
 @AllArgsConstructor
 public class LoginGetReturnDTO {
     private List<Post> total;
-    private List<LikeNumber> myLike;
+    private List<LikeListDTO> myLike;
 
 //    LoginGetReturnDTO(List<Post> total, List<LikeNumber> myLike){
 //        this.total = total;
