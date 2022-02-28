3797daf59028f1d97c42be707b16c3fa1abbee18
thdals83 <thdals83@naver.com>
2022-02-28 21:57:21 +0900

    AS-IS ) LikeNumber, User 엔티티 연관관계 작업 TO-BE ) LikeNUmber, Post 엔티티 연관관계 작업

--- src/main/java/com/sparta/spring_chapter3_2/controller/PostRestController.java
+++ src/main/java/com/sparta/spring_chapter3_2/controller/PostRestController.java
@@ -4,15 +4,13 @@ package com.sparta.spring_chapter3_2.controller;
 import com.sparta.spring_chapter3_2.dto.*;
 import com.sparta.spring_chapter3_2.model.LikeNumber;
 import com.sparta.spring_chapter3_2.model.Post;
-import com.sparta.spring_chapter3_2.model.User;
 import com.sparta.spring_chapter3_2.repository.LikeNumberRepository;
 import com.sparta.spring_chapter3_2.repository.PostRepository;
-import com.sparta.spring_chapter3_2.repository.UserRepository;
 import com.sparta.spring_chapter3_2.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -34,17 +32,26 @@ public class PostRestController {
         return returnDTO;
     }
 
-    //좋아요 다만들면 리턴 좋아요도 같이 해주기
+    //좋아요 다만들면 리턴 좋아요도 같이 해주기 원래대로면 Repository 들어가는건 서비스에서 해야 하지만 시간 없음
     @PostMapping("/api/showpost") //게시물 조회
     public LoginGetReturnDTO getPost(@RequestBody PostgetDTO postgetDTO){
         //아이디 없을 때
         if (postgetDTO.getUserId() == null){
-            List<LikeNumber> liketable = null;
-            LoginGetReturnDTO loginGetReturnDTO = new LoginGetReturnDTO(postRepository.findAllByOrderByModifiedAtDesc(), liketable);
+            List<LikeListDTO> likeListDTO = null;
+            LoginGetReturnDTO loginGetReturnDTO = new LoginGetReturnDTO(postRepository.findAllByOrderByModifiedAtDesc(), likeListDTO);
             return loginGetReturnDTO;
         }
+        //userid 넣는 작업
         List<LikeNumber> liketable = likeNumberRepository.findByUserId(postgetDTO.getUserId());
-        LoginGetReturnDTO loginGetReturnDTO = new LoginGetReturnDTO(postRepository.findAllByOrderByModifiedAtDesc(), liketable);
+        List<LikeListDTO> likeListDTO = new ArrayList<LikeListDTO>();
+
+        for (int i =0; i < liketable.size(); i++){
+            LikeListDTO likeListDTOtmp = new LikeListDTO(liketable.get(i));
+            likeListDTO.add(likeListDTOtmp);
+        }
+
+        //josn 총 입력 및 리턴턴
+       LoginGetReturnDTO loginGetReturnDTO = new LoginGetReturnDTO(postRepository.findAllByOrderByModifiedAtDesc(), likeListDTO);
         return loginGetReturnDTO;
     }
 
