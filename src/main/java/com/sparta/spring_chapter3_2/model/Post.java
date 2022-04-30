9d17cff144a3ce5e3376448e7b04dce48635e861
thdals83 <thdals83@naver.com>
2022-04-30 11:16:15 +0900

    AS-IS ) Builder작업 TO-BE ) 테스트 코드 작성

--- src/main/java/com/sparta/spring_chapter3_2/model/Post.java
+++ src/main/java/com/sparta/spring_chapter3_2/model/Post.java
@@ -43,7 +43,6 @@ public class Post extends Timestamped {
     @JsonBackReference
     private List<LikeNumber> likeNumber = new ArrayList<>();
 
-
     public void update(PostUpdateRequestDTO updateRequestDTO) {
         this.contents = updateRequestDTO.getContents();
         this.nickName = updateRequestDTO.getNickName();
